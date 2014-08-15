2dfdc8b4300932bf790d2abc4f0283f24810f08c
tumanoid <tumanoid@list.ru>
2014-08-15 14:27:23 +1100

    dll

--- common.h
+++ common.h
@@ -1,5 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+
 #ifdef WIN32
-#define DLL_EXPORT extern "C" __declspec ( dllimport )
+#define DLL_EXPORT extern "C" __declspec ( dllexport )
 #else
 #define DLL_EXPORT extern "C"
 #endif
+
+#endif // COMMON_H
--- main.cpp
+++ main.cpp
@@ -6,70 +6,124 @@
 #include <stdio.h>
 #include <iostream>
 
-#include "mysql_connection.h"
-#include "mysql_driver.h"
-
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
+#include "mysql.h"
 
 #include "common.h"
 
-static sql::Driver *driver;
-static sql::Connection *con;
-static sql::Statement *stmt;
-static sql::ResultSet *res;
+
+static MYSQL *sock;
+static MYSQL_RES *res;
+static char	qbuf [160];
+
+static FILE* err_f;
 
 
-extern "C" void sqlConnect ()
+DLL_EXPORT void sqlConnect ( const char* password )
 {
-	try
+	err_f = fopen ("err_out.txt", "w");
+	
+	if ( !( sock = mysql_init (0)))
 	{
-		driver = get_driver_instance ();
-		con = driver->connect ("tcp://188.120.224.62:3306","root", "DrCranch79");
-		con->setSchema ( "test" );
-		stmt = con->createStatement ();
+		fprintf ( err_f, "Couldn't initialize mysql struct\n" );
+		return;
 	}
-	catch ( sql::SQLException &e )
-    {
-		printf ("SQL err\n");
-		std::cout << e.what () << " ERR_CODE: " << e.getErrorCode () << std::endl;
-    }
-}
 
+	mysql_options ( sock,MYSQL_READ_DEFAULT_GROUP,"connect" );
 
-extern "C" void sqlQuerry ( const char* str )
-{	
-	try
+	if (!mysql_real_connect(sock, "188.120.224.62", "root", password, "test", 3306, NULL, 0 ))
+
 	{
-		//stmt = con->createStatement ();
+		fprintf ( err_f,"Couldn't connect to engine!\n%s\n", mysql_error ( sock ));
+		perror ( "" );
+		return;
+	}
 
-		stmt->execute ( str );
-		
-		res  = stmt->executeQuery ( "SELECT * FROM `ФИО`" );		
-		while ( res->next() )
-		{
-			printf ("%s %s %s %s \n", res->getString (1).c_str (),
-								res->getString (2).c_str (),
-								res->getString (3).c_str (),
-								res->getString (4).c_str ());					
-		}
-		delete res;
-		delete stmt;
-    }
-    catch ( sql::SQLException &e )
+	sock->reconnect = 1;
+
+	if ( mysql_select_db ( sock,"test" ))
+	{
+		fprintf ( err_f, "Couldn't select database test: Error: %s\n", mysql_error ( sock ));
+	}
+}
+
+
+DLL_EXPORT void sqlGetQuery ( const char* query_str, char* out_str )
+{	
+	mysql_query ( sock, query_str );
+	
+	if ( !( res = mysql_store_result ( sock )))
     {
-		printf ("SQL err\n");
-		std::cout << e.what () << " ERR_CODE: " << e.getErrorCode () << std::endl;
+      fprintf ( err_f, "Couldn't get result from %s\n", mysql_error ( sock ));
+	  return;
     }
-    
-    printf ( "---------------" );
-    
+	
+	MYSQL_ROW curr_row = mysql_fetch_row ( res );
+	sprintf ( out_str, "%s", curr_row [0] );
+	
+	mysql_free_result ( res );
+
+	fprintf ( err_f, out_str );	
 }
 
-extern "C" void dur ()
+
+DLL_EXPORT void sqlDisconnect ()
 {
-	printf ("***********LIB**********\n");
+	mysql_close(sock);
+	fclose ( err_f );
 }
 
+
+DLL_EXPORT void Dur ()
+{
+}
+/*
+int main(int argc, char **argv)
+{
+	printf ( "START \n" );
+
+
+  MYSQL_ROW row;
+
+
+  
+  char	qbuf[160];
+ // sprintf(qbuf,INSERT_QUERY,count,count);
+
+  //	INSERT
+  mysql_query ( sock, "INSERT INTO ФИО (Фамилия, Имя, Отчество) VALUES ('BUUUBO', 'Ванючая', 'МУПСТКА')" );
+
+  //	SELECT
+	mysql_query ( sock, "SELECT * FROM ФИО" );
+	
+	if (!(res=mysql_store_result(sock)))
+    {
+      fprintf(stderr,"Couldn't get result from %s\n",
+	      mysql_error(sock));
+      exit(1);
+    }
+
+    //printf ( "number of fields: %d\n", mysql_num_fields ( res ) );
+	int count_field = mysql_num_fields ( res );
+	int count_row   = mysql_num_rows ( res );
+
+	for ( int j=0;  j < count_row; j++)
+	{
+		row = mysql_fetch_row ( res );
+		for ( int i=0;  i < count_field; i++)
+		{
+			printf ("|  %s  |",  row [i]);
+		}
+		printf ("\n");
+	}
+    mysql_free_result(res);
+
+
+
+  mysql_close(sock);
+  
+	printf ( "END \n" );
+
+  exit(0);
+  return 0;
+}
+*/
